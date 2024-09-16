caacb028cea864bb1923ecabd007c5b3db8d9d16
Mustafa-Ashraf <mustafaashraf123@yahoo.com>
2024-09-16 17:40:18 +0300

    Add new features to the api
    Adding filter ,sort,limit fields and pagination to the app

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,91 +1,98 @@
-const Tour = require('../models/userModel');
+const User = require('../models/userModel');
+const APIFeatures = require('../utils/appFeatures');
 
 exports.createUser = async (req, res) => {
   try {
-    const newUser = await Tour.create(req.body);
+    const newUser = await User.create(req.body);
 
     res.status(201).json({
       status: 'success',
       data: {
-        user: newUser,
-      },
+        user: newUser
+      }
     });
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
 
 exports.getAllUsers = async (req, res) => {
   try {
-    const users = await Tour.find();
+    const features = new APIFeatures(User.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const users = await features.query;
 
     res.status(200).json({
       status: 'success',
       usersNumber: users.length,
       data: {
-        users,
-      },
+        users
+      }
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
 
 exports.getUser = async (req, res) => {
   try {
-    const user = await Tour.findById(req.params.id);
+    const user = await User.findById(req.params.id);
     res.status(200).json({
       status: 'success',
       data: {
-        user,
-      },
+        user
+      }
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
 
 exports.updateUser = async (req, res) => {
   try {
-    const updatedUser = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true,
+      runValidators: true
     });
 
     res.status(200).json({
       status: 'success',
       data: {
-        updatedUser,
-      },
+        updatedUser
+      }
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
 
 exports.deleteUser = async (req, res) => {
   try {
-    await Tour.findByIdAndDelete(req.params.id);
+    await User.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
-      data: null,
+      data: null
     });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     });
   }
 };
